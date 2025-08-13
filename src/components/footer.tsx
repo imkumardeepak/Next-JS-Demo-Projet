72a746e5cdeb45b162b662575dc772aa96398b1c
DEEPAK <deepakraznish@gmail.com>
2025-08-13 09:31:18 +0000

    in footer chnages it Aarkay Techno Consultants Pvt. Ltd. and chnges icon

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,14 @@
 
-import { Icons } from "@/components/icons";
+import Image from "next/image";
 
 export function Footer() {
   return (
     <footer className="w-full border-t bg-background">
       <div className="container flex flex-col items-center justify-center gap-4 py-5 md:h-20 md:flex-row md:py-0">
         <div className="flex items-center gap-2">
-           <Icons.logo className="h-5 w-5" />
+           <Image src="/logo.jpg" alt="Aarkay Techno Consultants Pvt. Ltd. Logo" width={20} height={20} className="h-5 w-5" />
           <p className="text-center text-sm leading-loose md:text-left">
-            © {new Date().getFullYear()} TransitPass. All Rights Reserved.
+            © {new Date().getFullYear()} Aarkay Techno Consultants Pvt. Ltd. All Rights Reserved.
           </p>
         </div>
       </div>
