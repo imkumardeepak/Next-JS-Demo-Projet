a6d8904255db580b12ac88fbefd35460dd9bb9fe
DEEPAK <deepakraznish@gmail.com>
2025-08-13 09:31:58 +0000

    make footer image width better

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <footer className="w-full border-t bg-background">
       <div className="container flex flex-col items-center justify-center gap-4 py-5 md:h-20 md:flex-row md:py-0">
         <div className="flex items-center gap-2">
-           <Image src="/logo.jpg" alt="Aarkay Techno Consultants Pvt. Ltd. Logo" width={20} height={20} className="h-5 w-5" />
+           <Image src="/logo.jpg" alt="Aarkay Techno Consultants Pvt. Ltd. Logo" width={32} height={32} className="h-8 w-8" />
           <p className="text-center text-sm leading-loose md:text-left">
             © {new Date().getFullYear()} Aarkay Techno Consultants Pvt. Ltd. All Rights Reserved.
           </p>
