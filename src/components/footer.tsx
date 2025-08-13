decaedb0e917f151df3fee4ff42b3cbd41822364
imkumardeepak <deepakraznish@gmail.com>
2025-08-13 15:06:05 +0530

    Adjusts footer logo width and reduces vertical padding
    Increases logo width for improved visibility and reduces footer
    vertical padding for a more compact appearance on smaller screens.
    Enhances visual alignment and consistency across devices.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,20 @@
-
 import Image from "next/image";
 
 export function Footer() {
   return (
     <footer className="w-full border-t bg-background">
-      <div className="container flex flex-col items-center justify-center gap-4 py-5 md:h-20 md:flex-row md:py-0">
+      <div className="container flex flex-col items-center justify-center gap-4 py-2 md:h-20 md:flex-row md:py-0">
         <div className="flex items-center gap-2">
-           <Image src="/logo.jpg" alt="Aarkay Techno Consultants Pvt. Ltd. Logo" width={32} height={32} className="h-8 w-8" />
+          <Image
+            src="/logo.jpg"
+            alt="Aarkay Techno Consultants Pvt. Ltd. Logo"
+            width={32}
+            height={32}
+            className="h-8 w-10"
+          />
           <p className="text-center text-sm leading-loose md:text-left">
-            © {new Date().getFullYear()} Aarkay Techno Consultants Pvt. Ltd. All Rights Reserved.
+            © {new Date().getFullYear()} Aarkay Techno Consultants Pvt. Ltd. All
+            Rights Reserved.
           </p>
         </div>
       </div>
