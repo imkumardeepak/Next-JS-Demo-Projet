b042d26f860bce3f82a9eb98aaad93fc8559278d
DEEPAK <deepakraznish@gmail.com>
2025-08-13 12:58:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Icons } from "@/components/icons";
 import { Footer } from "@/components/footer";
 import Image from "next/image";
